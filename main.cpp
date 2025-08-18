71f2d7934e5c8d9667df5b7acb2dc0d5c8ebd814
Ritham Sheti <sethiritham@gmail.com>
2025-08-18 14:45:00 +0530

    "Function overriding"

--- main.cpp
+++ main.cpp
@@ -3,32 +3,24 @@
 #include <string>
 #define print(x) std::cout<<x<<endl
 using namespace std;
-class Human{
+class Parent{
 public:
-	string Name;
-	int age;
-	string gender;
-	string nationality;
-	string religion;
-	Human(string Name, int age, string gender) {
-		this->age = age;
-		this->Name = Name;
-		this->gender = gender;
-	}
+	void getInfo() {
+		print("Parent class\n");
 
-	void displayInfo() 
-	{
-		cout << "I am " << Name << ", I am " << age << " years old, " << " I am a " << gender << endl;
-	}
-	void displayInfo(string nationality, string religion)
-	{
-		cout << "I am from " << nationality << " I am a follower of " << religion;
 	}
 
 };
+class Child : public Parent {
+public:
+	void getInfo() {
+		print("Child class\n");
+	}
+};
 int main() {
-	Human h1("Ritham", 18, "Male");
-	h1.displayInfo();
-	h1.displayInfo("India", "Jainsim");
-	
+	Child c1;
+	c1.getInfo();
+	Parent p1;
+	p1.getInfo();
+
 }
